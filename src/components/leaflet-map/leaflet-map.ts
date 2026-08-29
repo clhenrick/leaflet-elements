6b6195b96290186ba8d075ea6ae15a5d03488599
Chris Henrick <chrishenrick@gmail.com>
2026-08-29 10:24:02 -0700

    feat: handle updates to props center & zoom

--- src/components/leaflet-map/leaflet-map.ts
+++ src/components/leaflet-map/leaflet-map.ts
@@ -1,4 +1,4 @@
-import { LitElement, html, unsafeCSS } from "lit";
+import { LitElement, html, unsafeCSS, type PropertyValues } from "lit";
 import { customElement, property, query } from "lit/decorators.js";
 import { Map, TileLayer, type LatLngTuple } from "leaflet";
 
@@ -9,15 +9,28 @@ import styles from "./leaflet-map.css?inline";
 
 @customElement("leaflet-map")
 export class LeafletMap extends LitElement {
+  // #region static
   static styles = [
     // unsafeCSS(leafletStyles),
     unsafeCSS(styles),
   ];
 
+  //#endregion
+
+  //#region private properties
+
   #map: Map | null = null;
+
+  //#endregion
+
+  //#region shadow dom queries
 
   @query("#map")
   container!: HTMLElement;
+
+  //#endregion
+
+  //#region public properties
 
   @property({ attribute: false }) tileLayer!: TileLayer;
 
@@ -25,7 +38,12 @@ export class LeafletMap extends LitElement {
 
   @property({ type: Number, reflect: true }) zoom = 12;
 
+  // #endregion
+
+  //#region lifecycle methods
+
   firstUpdated(): void {
+    // TODO: move to connectedCallback?
     if (!this.#map) {
       this.#map = new Map(this.container).setView(this.center, this.zoom);
     }
@@ -41,9 +59,28 @@ export class LeafletMap extends LitElement {
     }
   }
 
+  protected updated(changedProperties: PropertyValues<this>): void {
+    if (changedProperties.has("center")) {
+      this.#map!.setView(this.center);
+    }
+
+    if (changedProperties.has("zoom")) {
+      this.#map!.setView(this.center, this.zoom);
+    }
+
+    if (changedProperties.has("tileLayer")) {
+      // TODO
+    }
+  }
+
+  //#endregion
+
+  //#region rendering
+
   private _renderLeafletStylesLink() {
     return html`<link
       rel="stylesheet"
+      // TODO: add property for specifying styles URL?
       href="https://unpkg.com/leaflet@2.0.0-alpha.1/dist/leaflet.css"
     />`;
   }
@@ -55,4 +92,6 @@ export class LeafletMap extends LitElement {
   render() {
     return html`${this._renderLeafletStylesLink()} ${this._renderMapContainer()}`;
   }
+
+  //#endregion
 }
