c43d62e5101a4af7a9c752bb5eebdf1f3c844c06
Chris Henrick <chrishenrick@gmail.com>
2026-08-30 17:23:42 -0700

    fix: type error

--- src/components/leaflet-map/leaflet-map.test.ts
+++ src/components/leaflet-map/leaflet-map.test.ts
@@ -4,8 +4,9 @@ import { html } from "lit";
 import "./leaflet-map";
 
 test("renders without errors", async () => {
+  const center = [37.8, -122.27] as const;
   const { getByRole } = render(
-    html`<leaflet-map .center=${[37.8, -122.27]} zoom=${12}></leaflet-map>`,
+    html`<leaflet-map .center=${center} zoom=${12}></leaflet-map>`,
   );
 
   await expect.element(getByRole("application")).toBeInTheDocument();
